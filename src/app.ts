fcc891c9a635df2cd7682345100e7a98a655393b
Eduardo Kitamura Manrique <eduardo.manrique@gmail.com>
2023-11-16 14:46:08 +0000

    Update /get endpoint and add / POST endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 const port = 3000;
 
+// Updated GET endpoint to return JSON
 app.get('/get', (req, res) => {
-  res.send('Hello');
+  res.json({ message: 'Hello World!' });
+});
+
+// New POST endpoint
+app.post('/', (req, res) => {
+  // For now, just return status 200
+  res.status(200).send();
 });
 
 app.listen(port, () => {
