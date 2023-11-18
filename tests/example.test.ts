c118f24ff185338eed2eeadc7269a3e9a985fce6
leovcunha <lvcunha@gmail.com>
2023-11-18 13:07:15 +0000

    separation of concerns

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -1,23 +1,23 @@
-import request from 'supertest';
-import app from '../src/app';
-import { Server } from 'http';
+import request from "supertest";
+import app from "../src/app";
+import { Server } from "http";
 
-describe('Example Test Suite', () => {
-  let server: Server;
+describe("Example Test Suite", () => {
+    // let server: Server;
 
-  // Start the server before the tests run
-  beforeAll((done) => {
-    server = app.listen(3000, done);
-  });
+    // // Start the server before the tests run
+    // beforeAll((done) => {
+    //     server = app.listen(3000, done);
+    // });
 
-  // Close the server after the tests run
-  afterAll((done) => {
-    server.close(done);
-  });
+    // // Close the server after the tests run
+    // afterAll((done) => {
+    //     server.close(done);
+    // });
 
-  it('GET /get - success', async () => {
-    const result = await request(server).get('/get');
-    expect(result.statusCode).toBe(200);
-    expect(result.body).toEqual({ message: 'Hello World!' });
-  });
+    it("GET /get - success", async () => {
+        const result = await request(app).get("/get");
+        expect(result.statusCode).toBe(200);
+        expect(result.body).toEqual({ message: "Hello World!" });
+    });
 });
