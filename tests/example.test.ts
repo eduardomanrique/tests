fcf838dbf95762080f0c50ac27a6e1b89df9c507
Eduardo Kitamura Manrique <eduardo.manrique@gmail.com>
2023-11-18 12:41:30 +0000

    Update tests for proper async handling

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -2,8 +2,20 @@ const request = require('supertest');
 const app = require('../src/app');
 
 describe('Example Test Suite', () => {
+  let server;
+
+  // Start the server before the tests run
+  beforeAll((done) => {
+    server = app.listen(3000, done);
+  });
+
+  // Close the server after the tests run
+  afterAll((done) => {
+    server.close(done);
+  });
+
   it('GET /get - success', async () => {
-    const result = await request(app).get('/get');
+    const result = await request(server).get('/get');
     expect(result.statusCode).toBe(200);
     expect(result.body).toEqual({ message: 'Hello World!' });
   });
