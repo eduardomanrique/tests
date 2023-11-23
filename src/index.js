23938a0540e830e0a9436dc087edf7b26d6159e7
Eduardo Kitamura Manrique <eduardo.manrique@gmail.com>
2023-11-23 12:43:16 +0100

    Create home page with link to Snake game

--- src/index.js
+++ src/index.js
@@ -2,10 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+const HomePage = () => (
+  <div style={{ textAlign: 'center', marginTop: '50px' }}>
+    <h1>Welcome to the Game Portal</h1>
+    <Link to="/snake-game">Play Snake Game</Link>
+  </div>
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route path="/snake-game">
+          <App />
+        </Route>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
